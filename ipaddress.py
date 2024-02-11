23fd0e03fca801fc6b6c17b8f29596e1ced70bb6
divinekaremera <m.karemera@alustudent.com>
2024-02-11 11:34:40 +0200

    IP Address Project

---
+++ ipaddress.py
@@ -0,0 +1,7 @@
+import re
+
+if __name__ == "__main__":
+    test_string = input("Enter your data: ")
+    patterns = r'\b(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b'
+    matches = re.findall(patterns, test_string)
+    print('Search results: ', matches)
