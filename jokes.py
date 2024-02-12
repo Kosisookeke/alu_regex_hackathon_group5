9b9ff31547e73ce1c3fd1cf8364854d6968794d7
Irenee123 <i.dusingizi@alustudent.com>
2024-02-12 19:25:06 +0000

    Regex Onboarding

--- jokes.py
+++ jokes.py
@@ -1,7 +1,11 @@
 import re
 
-if __name__ == "__main__":
-    test_string = input("Enter your data: ")
-    patterns = r'Why did the .*? Because.*?'
-    matches = re.findall(patterns, test_string)
-    print('Search results: ', matches)
+string = "Why did the chicken cross the road? Because it wanted to get to the other side"
+
+pattern = r"Why did the (.*?)\? Because(.*?)"
+
+match = re.search(pattern, string)
+
+if match:
+    print("Joke Setup:", match.group(1))
+    print("Joke Punchline:", match.group(2))
